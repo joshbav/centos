38fe991083955958dc00395769475c055ba4a942
“joshbav” <josh@Joshs-Mac.home>
2018-02-07 12:17:11 -0800

    scripted commit

--- build.sh
+++ build.sh
@@ -5,3 +5,22 @@ echo
 echo Pushing newly built image to dockerhub
 echo
 docker push joshbav/centos:latest
+echo
+echo
+echo Uploading all files to github.com/joshbav/tractor
+echo
+# ALl files to automatically be added
+git add .
+git config user.name “joshbav”
+git commit -m "scripted commit"
+git push -u origin master
+
+
+
+
+
+
+
+
+
+
